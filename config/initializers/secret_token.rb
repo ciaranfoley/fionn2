3a3719df09f20715aad2ac5b9c669b5fbe4aefbf
cookr <ciaran@cookr.com>
2012-12-20 13:22:04 -0500

    Project Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fionn2::Application.config.secret_token = '5c311d53b62154ba194584db95471236a64c6ffe02ffeeabc57a84d35311a3aed2981a0bb21d114fa9770d5247cffd8854680256a9f1278fc9cd7c0bbd971c3d'
